fb6f64dac76c7c41b1ed64281b2e3d7b326b1122
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-10 20:58:45 +0700

    delete canvas setting

--- src/app/Views/home/main.php
+++ src/app/Views/home/main.php
@@ -96,12 +96,6 @@
             </div>
         </div>
 
-        <div class="customizer-setting d-none d-md-block">
-            <div class="btn-info rounded-pill shadow-lg btn btn-icon btn-lg p-2" data-bs-toggle="offcanvas" data-bs-target="#theme-settings-offcanvas" aria-controls="theme-settings-offcanvas">
-                <i class='mdi mdi-spin mdi-cog-outline fs-22'></i>
-            </div>
-        </div>
-
         <!-- JAVASCRIPT -->
         <script src="<?= base_url('dist/') ?>assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
         <script src="<?= base_url('dist/') ?>assets/libs/simplebar/simplebar.min.js"></script>
