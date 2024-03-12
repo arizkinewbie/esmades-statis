aedf157d98f58fcd03493bfae82389d3ef60787e
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-03-12 15:56:13 +0700

    feat : add modal kelembagaan

--- src/app/Views/home/kelembagaan.php
+++ src/app/Views/home/kelembagaan.php
@@ -1,5 +1,5 @@
-<!-- Full screen modal content -->
-<div class="modal fade flip modalFullScreen" tabindex="-1" aria-labelledby="modalFullScreenLabel" aria-hidden="true">
+<!-- Sample Modal -->
+<div class="modal fade flip modalFullScreen" id="bpdModal" tabindex="-1" aria-labelledby="modalFullScreenLabel" aria-hidden="true">
    <div class="modal-dialog modal-xl modal-dialog-scrollable">
       <div class="modal-content">
          <div class="modal-header">
@@ -35,6 +35,92 @@
    </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
+<!-- sample modal 2 -->
+<div class="modal fade zoomIn modalKelembagaan" id="modalSample2" tabindex="-1" aria-labelledby="modalKelembagaanLabel" aria-hidden="true">
+   <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="modalKelembagaanLabel">Kelembagaan</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+            <div class="d-flex justify-content-center mt-4">
+               <div class="row">
+                  <div class="col-md-4 text-center">
+                     <div class="text-center">
+                        <a href="javascript:showModal('BPD')">
+                           <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
+                              <img src="<?= base_url('dist/') ?>assets/images/lembaga/bpd.png" alt="" class="avatar-sm material-shadow">
+                           </div>
+                           <h5 class="fs-14 mb-1">BPD</h5>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-4 text-center">
+                     <div class="text-center">
+                        <a href="javascript:showModalGalery()">
+                           <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
+                              <img src="<?= base_url('dist/') ?>assets/images/lembaga/lpm.png" alt="" class="avatar-sm material-shadow">
+                           </div>
+                           <h5 class="fs-14 mb-1">LPM</h5>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-4 text-center">
+                     <div class="text-center">
+                        <a href="javascript:showModalISO()">
+                           <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
+                              <img src="<?= base_url('dist/') ?>assets/images/lembaga/bumdes.png" alt="" class="avatar-sm material-shadow">
+                           </div>
+                           <h5 class="fs-14 mb-1">BUMDES</h5>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-4 text-center">
+                     <div class="text-center">
+                        <a href="javascript:showModalNews()">
+                           <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
+                              <img src="<?= base_url('dist/') ?>assets/images/lembaga/pkk.png" alt="" class="avatar-sm material-shadow">
+                           </div>
+                           <h5 class="fs-14 mb-1">PKK</h5>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-4 text-center">
+                     <div class="text-center">
+                        <a href="javascript:showModalNews()">
+                           <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
+                              <img src="<?= base_url('dist/') ?>assets/images/lembaga/karang-taruna.png" alt="" class="avatar-sm material-shadow">
+                           </div>
+                           <h5 class="fs-14 mb-1">Karang Taruna</h5>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-4 text-center">
+                     <div class="text-center">
+                        <a href="javascript:showModalNews()">
+                           <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
+                              <img src="<?= base_url('dist/') ?>assets/images/lembaga/posyandu.png" alt="" class="avatar-sm material-shadow">
+                           </div>
+                           <h5 class="fs-14 mb-1">Kader Posyandu</h5>
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12">
+
+               </div>
+            </div>
+         </div>
+         <div class="modal-footer">
+            <a href="javascript:void(0);" class="btn btn-link link-success fw-medium material-shadow-none" data-bs-dismiss="modal"><i class="ri-close-line me-1 align-middle"></i> Close</a>
+         </div>
+      </div><!-- /.modal-content -->
+   </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 <div class="container-fluid py-5 mx-2" style="padding-left: 15px; padding-right: 15px; margin-top: 45px;">
    <div class="row justify-content-center">
       <div class="col-lg-9 offset-lg-2 col-md-10 offset-md-1">
@@ -47,7 +133,7 @@
                   <div class="col-md-6"> <!-- Sesuaikan ukuran kolom dengan kebutuhan -->
                      <div class="row">
                         <div class="col-md-4 text-center mb-4">
-                           <a href="javascript:showModal('BPD')">
+                           <a href="#" data-bs-toggle="modal" data-bs-target="#bpdModal"> <!--Ganti pada data-bs-target sesuaikan dengan id modal yang ingin dipanggil -->
                               <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
                                  <img src="<?= base_url('dist/') ?>assets/images/lembaga/bpd.png" alt="" class="avatar-sm material-shadow">
                               </div>
@@ -55,7 +141,7 @@
                            </a>
                         </div>
                         <div class="col-md-4 text-center mb-4">
-                           <a href="javascript:showModalGalery()">
+                           <a href="#" data-bs-toggle="modal" data-bs-target="#modalSample2"> <!--Ganti pada data-bs-target sesuaikan dengan id modal yang ingin dipanggil -->
                               <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
                                  <img src="<?= base_url('dist/') ?>assets/images/lembaga/lpm.png" alt="" class="avatar-sm material-shadow">
                               </div>
@@ -64,7 +150,7 @@
                         </div>
                         <div class="col-md-4 text-center">
                            <div class="text-center">
-                              <a href="javascript:showModalISO()">
+                              <a href="#" data-bs-toggle="modal" data-bs-target="#bpdModal"> <!--Ganti pada data-bs-target sesuaikan dengan id modal yang ingin dipanggil -->
                                  <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
                                     <img src="<?= base_url('dist/') ?>assets/images/lembaga/bumdes.png" alt="" class="avatar-sm material-shadow">
                                  </div>
@@ -74,7 +160,7 @@
                         </div>
                         <div class="col-md-4 text-center">
                            <div class="text-center">
-                              <a href="javascript:showModalNews()">
+                              <a href="#" data-bs-toggle="modal" data-bs-target="#bpdModal"> <!--Ganti pada data-bs-target sesuaikan dengan id modal yang ingin dipanggil -->
                                  <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
                                     <img src="<?= base_url('dist/') ?>assets/images/lembaga/pkk.png" alt="" class="avatar-sm material-shadow">
                                  </div>
@@ -84,7 +170,7 @@
                         </div>
                         <div class="col-md-4 text-center">
                            <div class="text-center">
-                              <a href="javascript:showModalNews()">
+                              <a href="#" data-bs-toggle="modal" data-bs-target="#modalSample2"> <!--Ganti pada data-bs-target sesuaikan dengan id modal yang ingin dipanggil -->
                                  <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
                                     <img src="<?= base_url('dist/') ?>assets/images/lembaga/karang-taruna.png" alt="" class="avatar-sm material-shadow">
                                  </div>
@@ -94,7 +180,7 @@
                         </div>
                         <div class="col-md-4 text-center">
                            <div class="text-center">
-                              <a href="javascript:showModalNews()">
+                              <a href="#" data-bs-toggle="modal" data-bs-target="#bpdModal"> <!--Ganti pada data-bs-target sesuaikan dengan id modal yang ingin dipanggil -->
                                  <div class="profile-user position-relative d-inline-block mx-auto mb-2 mt-3">
                                     <img src="<?= base_url('dist/') ?>assets/images/lembaga/posyandu.png" alt="" class="avatar-sm material-shadow">
                                  </div>
