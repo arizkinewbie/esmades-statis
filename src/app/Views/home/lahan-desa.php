cfc0eea6970ecb97f304859253418a1b17a371d5
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-12 15:20:41 +0700

    add file for sidebar

---
+++ src/app/Views/home/lahan-desa.php
@@ -0,0 +1,13 @@
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header d-flex align-items-center flex-wrap">
+                    <h2 class="mb-0">Penggunaan Lahan Desa</h2>
+                </div>
+                <div class="card-body">
+                    <!--  Content here -->
+                </div>
+            </div>
+        </div>
+    </div>
