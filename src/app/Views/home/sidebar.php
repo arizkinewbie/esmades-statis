588649e902bf8506a8b093eb367a2429206233e7
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-12 15:05:24 +0700

    change sidebar peta desa

--- src/app/Views/home/sidebar.php
+++ src/app/Views/home/sidebar.php
@@ -1,22 +1,12 @@
 <div class="app-menu navbar-menu">
     <!-- LOGO -->
     <div class="navbar-brand-box bg-success">
-        <!-- Dark Logo-->
-        <a href="index.html" class="logo logo-dark">
+        <a href="<?= base_url('') ?>" class="logo logo-light">
             <span class="logo-sm">
                 <img src="<?= base_url('dist/') ?>assets/images/logo-sm.png" alt="" height="22">
             </span>
             <span class="logo-lg">
-                <img src="<?= base_url('dist/') ?>assets/images/logo-dark.png" alt="" height="17">
-            </span>
-        </a>
-        <!-- Light Logo-->
-        <a href="index.html" class="logo logo-light">
-            <span class="logo-sm">
-                <img src="<?= base_url('dist/') ?>assets/images/logo-sm.png" alt="" height="22">
-            </span>
-            <span class="logo-lg">
-                <img src="<?= base_url('dist/') ?>assets/images/logo-light.png" class="mt-2 mb-2" alt="" height="50">
+                <img src="<?= base_url('dist/') ?>assets/images/logo-dark.png" class="mt-2 mb-2" alt="" height="50">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
@@ -31,10 +21,10 @@
                     <div class="mx-auto">
                         <img src="<?= base_url('dist/') ?>assets/images/logo_desa_talaga.png" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
-                    <p class="text-light mt-0">Profil Desa Talaga Wetan</p>
+                    <p class="text-light mt-0">Profil Desa <?= $nama ?></p>
                 </a>
 
-                <a href="<?= base_url('profil') ?>" class="card-body text-center" style="flex:0 0 50%;">
+                <a href="<?= base_url('kepala-desa') ?>" class="card-body text-center" style="flex:0 0 50%;">
                     <div class="mx-auto">
                         <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
@@ -46,7 +36,7 @@
                     <div class="mx-auto">
                         <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
-                    <p class="text-light mt-0">Struktur Organisasi</p>
+                    <p class="text-light mt-0">Struktur Organisasi Desa</p>
                 </a>
 
                 <a href="<?= base_url('kelembagaan') ?>" class="card-body text-center" style="flex:0 0 50%;">
@@ -71,8 +61,8 @@
                 </a>
             </div>
 
-
             <ul class="navbar-nav" id="navbar-nav">
+                <!-- Peta Desa -->
                 <li class="menu-title"><span data-key="t-menu">Peta Desa</span></li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="<?= base_url('aset-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
@@ -80,53 +70,33 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('tanah-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-flood-line"></i> <span data-key="t-layouts">Tanah Desa</span>
+                    <a class="nav-link menu-link" href="<?= base_url('lahan-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                        <i class="ri-flood-line"></i> <span data-key="t-layouts">Penggunaan Lahan Desa</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('kependudukan') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-map-pin-user-line"></i> <span data-key="t-layouts">Kependudukan</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Baru</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('cagar-budaya') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-earth-line"></i> <span data-key="t-layouts">Cagar Budaya</span>
+                    <a class="nav-link menu-link" href="<?= base_url('sapras-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                        <i class="ri-earth-line"></i> <span data-key="t-layouts">Sarana dan Prasarana Desa</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="<?= base_url('objek-wisata') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-ship-2-line"></i> <span data-key="t-layouts">Objek Wisata</span>
+                        <i class="ri-ship-2-line"></i> <span data-key="t-layouts">Informasi Potensi Wisata Desa</span>
                     </a>
-                </li> <!-- end Dashboard Menu -->
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="<?= base_url('kependudukan') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                        <i class="ri-map-pin-user-line"></i> <span data-key="t-layouts">Sebaran Penduduk</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Baru</span>
+                    </a>
+                </li>
 
+                <!-- Rekapitulasi Desa -->
                 <li class="menu-title"><span data-key="t-menu">Rekapitulasi Data</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarLayouts" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-home-3-line"></i> <span data-key="t-layouts">Aset Desa</span>
+                    <a class="nav-link menu-link" href="<?= base_url('peraturan-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                        <i class="ri-ship-2-line"></i> <span data-key="t-layouts">Peraturan/Keputusan Desa</span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarLayouts" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-flood-line"></i> <span data-key="t-layouts">Tanah Desa</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarLayouts" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-map-pin-user-line"></i> <span data-key="t-layouts">Kependudukan</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Baru</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarLayouts" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-earth-line"></i> <span data-key="t-layouts">Cagar Budaya</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarLayouts" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-ship-2-line"></i> <span data-key="t-layouts">Objek Wisata</span>
-                    </a>
-                </li>
-
             </ul>
         </div>
         <!-- Sidebar -->
