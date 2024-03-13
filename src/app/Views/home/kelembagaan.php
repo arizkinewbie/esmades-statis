b076efadb2975a0051103824e7f2ffb76c7d8ddf
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-03-14 01:02:44 +0700

    fix : kelembagaan error

--- src/app/Views/home/kelembagaan.php
+++ src/app/Views/home/kelembagaan.php
@@ -1,31 +1,230 @@
-<!-- Sample Modal -->
-<div class="modal fade flip modalFullScreen" id="bpdModal" tabindex="-1" aria-labelledby="modalFullScreenLabel" aria-hidden="true">
+<!-- Full screen modal content -->
+<div class="modal fade zoomIn modalNews" id="bpdModal" tabindex="-1" aria-labelledby="modalNewsLabel" aria-hidden="true">
    <div class="modal-dialog modal-xl modal-dialog-scrollable">
       <div class="modal-content">
          <div class="modal-header">
-            <h5 class="modal-title" id="modalFullScreenLabel">Full Screen Modal</h5>
+            <h5 class="modal-title" id="modalNewsLabel">Berita</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
          </div>
          <div class="modal-body">
-            <h6 class="fs-15">Sejarah</h6>
-            <div class="d-flex">
-               <div class="flex-grow-1 ms-2">
-                  <p class="text-muted mb-0">
-                     <?= $sejarah ?>
-                  </p>
+            <div class="row">
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-1.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-success-subtle text-success fs-11">Business</span>
+                                 </li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">A mix of friends and strangers heading off to
+                                    find an adventure</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> James
+                                    Ballard</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 23
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
                </div>
-            </div>
-            <h6 class="fs-16 my-3">Visi</h6>
-            <div class="d-flex mt-3">
-               <div class="flex-grow-1 ms-2 ">
-                  <p class="text-muted mb-0"><?= $visi ?></p>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-2.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-warning-subtle text-warning fs-11">Development</span>
+                                 </li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">How to get creative in your work ?</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Ruby
+                                    Griffin</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 23
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
                </div>
-            </div>
-            <h6 class="fs-16 my-3">Misi</h6>
-            <div class="d-flex mt-3">
-               <div class="flex-grow-1 ms-2 ">
-                  <p class="text-muted mb-0"><?= $misi ?></p>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-3.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-info-subtle text-info fs-11">Fashion</span></li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">How to become a best sale marketer in a
+                                    year!</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Elwood
+                                    Arter</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 23
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
                </div>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-4.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-primary-subtle text-primary fs-11">Product</span>
+                                 </li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">Manage white space in responsive layouts ?</a>
+                              </h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Nancy
+                                    Martino</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 23
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
+               </div>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-5.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-success-subtle text-success fs-11">Business</span>
+                                 </li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">Stack designer Olivia Murphy offers
+                                    freelancing advice</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Erica
+                                    Kernan</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 11
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
+               </div>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-6.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-danger-subtle text-danger fs-11">Design</span></li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">A day in the of a professional fashion
+                                    designer</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Jason
+                                    McQuaid</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 14
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
+               </div>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-7.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-danger-subtle text-danger fs-11">Design</span></li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">Design your apps in your own way</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Henry
+                                    Baird</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 19
+                                    Nov, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
+               </div>
+               <!--end col-->
+
+               <div class="col-lg-6">
+                  <div class="card border">
+                     <div class="card-body">
+                        <div class="d-sm-flex">
+                           <div class="flex-shrink-0">
+                              <img src="<?= base_url('dist/') ?>assets/images/small/img-8.jpg" alt="" width="115" class="rounded-1" />
+                           </div>
+                           <div class="flex-grow-1 ms-sm-4 mt-3 mt-sm-0">
+                              <ul class="list-inline mb-2 mt-3">
+                                 <li class="list-inline-item"><span class="badge bg-warning-subtle text-warning fs-11">Development</span>
+                                 </li>
+                              </ul>
+                              <h5><a href="javascript:void(0);">How apps is changing the IT world</a></h5>
+                              <ul class="list-inline mb-0">
+                                 <li class="list-inline-item"><i class="ri-user-3-fill text-success align-middle me-1"></i> Elwood
+                                    Arter</li>
+                                 <li class="list-inline-item"><i class="ri-calendar-2-fill text-success align-middle me-1"></i> 10
+                                    Aug, 2021</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!--end card-->
+               </div>
+               <!--end col-->
             </div>
          </div>
          <div class="modal-footer">
