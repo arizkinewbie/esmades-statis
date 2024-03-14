62c0a219ef019a97dac46b38f35c6b89cd9e6ce6
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-14 12:42:16 +0700

    add: tabel

--- src/app/Views/home/peraturan-desa.php
+++ src/app/Views/home/peraturan-desa.php
@@ -6,7 +6,42 @@
                     <h2 class="mb-0">Peraturan Desa</h2>
                 </div>
                 <div class="card-body">
-                    <!--  Content here -->
+                    <table id="scroll-horizontal" class="table nowrap align-middle" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Jenis</th>
+                                <th>Nomor</th>
+                                <th>Tahun</th>
+                                <th>Tentang</th>
+                                <th>Status</th>
+                                <th>File</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Peraturan Desa</td>
+                                <td>05</td>
+                                <td>2022</td>
+                                <td>Penetapan Keluarga Penerima Manfaat Bantuan Langsung Tunai (BLT) Tahun 2023</td>
+                                <td>Aktif</td>
+                                <td>
+                                    <a href="#" class="btn btn-secondary btn-sm"><i class="mdi mdi-eye-outline"></i> Lihat</a>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="mdi mdi-download"></i> Unduh</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Peraturan Menteri</td>
+                                <td>114</td>
+                                <td>2014</td>
+                                <td>Pedoman Pembangunan Desa</td>
+                                <td>Aktif</td>
+                                <td>
+                                    <a href="#" class="btn btn-secondary btn-sm"><i class="mdi mdi-eye-outline"></i> Lihat</a>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="mdi mdi-download"></i> Unduh</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
