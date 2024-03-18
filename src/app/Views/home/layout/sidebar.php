7c0cb42fdd22fd6c1cdd462a268c1ac223a8a225
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-18 13:40:26 +0700

    feat: add menu icon

--- src/app/Views/home/layout/sidebar.php
+++ src/app/Views/home/layout/sidebar.php
@@ -9,8 +9,7 @@
                 <img src="<?= base_url('dist/') ?>assets/images/logo-dark.png" class="mt-2 mb-2" alt="" height="50">
             </span>
         </a>
-        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
-            id="vertical-hover">
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
             <i class="ri-record-circle-line"></i>
         </button>
     </div>
@@ -20,16 +19,14 @@
             <div id="two-column-menu" class="mt-2 d-flex flex-wrap justify-content-center">
                 <a href="<?= base_url('profil') ?>" class="card-body text-center">
                     <div class="mx-auto">
-                        <img src="<?= base_url('dist/') ?>assets/images/logo_desa_talaga.png" alt="" width="50px"
-                            class="img-thumbnail rounded-circle shadow-none">
+                        <img src="<?= base_url('dist/') ?>assets/images/logo_desa_talaga.png" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
                     <p class="text-light mt-0">Profil Desa <?= $data[0]['nama'] ?></p>
                 </a>
 
                 <a href="<?= base_url('kepala-desa') ?>" class="card-body text-center" style="flex:0 0 50%;">
                     <div class="mx-auto">
-                        <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px"
-                            class="img-thumbnail rounded-circle shadow-none">
+                        <img src="<?= base_url('dist/') ?>assets/images/menu/kepala-desa.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
                     <p class="text-light mt-0">Kepala Desa</p>
                 </a>
@@ -37,32 +34,28 @@
 
                 <a href="<?= base_url('struktur-organisasi') ?>" class="card-body text-center" style="flex:0 0 50%;">
                     <div class="mx-auto">
-                        <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px"
-                            class="img-thumbnail rounded-circle shadow-none">
+                        <img src="<?= base_url('dist/') ?>assets/images/menu/struktur-organisasi.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
                     <p class="text-light mt-0">Struktur Organisasi Desa</p>
                 </a>
 
                 <a href="<?= base_url('kelembagaan') ?>" class="card-body text-center" style="flex:0 0 50%;">
                     <div class="mx-auto">
-                        <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px"
-                            class="img-thumbnail rounded-circle shadow-none">
+                        <img src="<?= base_url('dist/') ?>assets/images/menu/kelembagaan.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
                     <p class="text-light mt-0">Kelembagaan</p>
                 </a>
 
                 <a href="<?= base_url('galeri') ?>" class="card-body text-center" style="flex:0 0 50%;">
                     <div class="mx-auto">
-                        <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px"
-                            class="img-thumbnail rounded-circle shadow-none">
+                        <img src="<?= base_url('dist/') ?>assets/images/menu/galeri.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
                     <p class="text-light mt-0">Galeri</p>
                 </a>
 
                 <a href="<?= base_url('kabar-desa') ?>" class="card-body text-center" style="flex:0 0 50%;">
                     <div class="mx-auto">
-                        <img src="<?= base_url('dist/') ?>assets/images/users/avatar-10.jpg" alt="" width="50px"
-                            class="img-thumbnail rounded-circle shadow-none">
+                        <img src="<?= base_url('dist/') ?>assets/images/menu/kabar-desa.jpg" alt="" width="50px" class="img-thumbnail rounded-circle shadow-none">
                     </div>
                     <p class="text-light mt-0">Kabar Desa</p>
                 </a>
@@ -72,42 +65,35 @@
                 <!-- Peta Desa -->
                 <li class="menu-title"><span data-key="t-menu">Peta Desa</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('aset-desa') ?>" role="button"
-                        aria-expanded="false" aria-controls="sidebarLayouts">
+                    <a class="nav-link menu-link" href="<?= base_url('aset-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
                         <i class="ri-home-3-line"></i> <span data-key="t-layouts">Aset Desa</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('lahan-desa') ?>" role="button"
-                        aria-expanded="false" aria-controls="sidebarLayouts">
+                    <a class="nav-link menu-link" href="<?= base_url('lahan-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
                         <i class="ri-flood-line"></i> <span data-key="t-layouts">Penggunaan Lahan Desa</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('sapras-desa') ?>" role="button"
-                        aria-expanded="false" aria-controls="sidebarLayouts">
+                    <a class="nav-link menu-link" href="<?= base_url('sapras-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
                         <i class="ri-earth-line"></i> <span data-key="t-layouts">Sarana dan Prasarana Desa</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('objek-wisata') ?>" role="button"
-                        aria-expanded="false" aria-controls="sidebarLayouts">
+                    <a class="nav-link menu-link" href="<?= base_url('objek-wisata') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
                         <i class="ri-ship-2-line"></i> <span data-key="t-layouts">Informasi Potensi Wisata Desa</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('kependudukan') ?>" role="button"
-                        aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i class="ri-map-pin-user-line"></i> <span data-key="t-layouts">Sebaran Penduduk</span> <span
-                            class="badge badge-pill bg-danger" data-key="t-hot">Baru</span>
+                    <a class="nav-link menu-link" href="<?= base_url('kependudukan') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                        <i class="ri-map-pin-user-line"></i> <span data-key="t-layouts">Sebaran Penduduk</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Baru</span>
                     </a>
                 </li>
 
                 <!-- Rekapitulasi Desa -->
                 <li class="menu-title"><span data-key="t-menu">Rekapitulasi Data</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="<?= base_url('peraturan-desa') ?>" role="button"
-                        aria-expanded="false" aria-controls="sidebarLayouts">
+                    <a class="nav-link menu-link" href="<?= base_url('peraturan-desa') ?>" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
                         <i class="ri-ship-2-line"></i> <span data-key="t-layouts">Peraturan/Keputusan Desa</span>
                     </a>
                 </li>
