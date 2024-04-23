c3b83aea32a63a92dbc613c8aecb5635a538e897
arizkinewbie <arizkijitsu@gmail.com>
2024-04-23 14:24:49 +0700

    add datatable

--- src/app/Views/home/struktur-organisasi.php
+++ src/app/Views/home/struktur-organisasi.php
@@ -1,22 +1,54 @@
 <div class="container-fluid py-5" style="padding-left: 15px; padding-right: 15px; margin-top: 45px;">
-   <div class="row justify-content-center">
-      <!-- Mengatur ulang lebar kolom dan offset untuk desktop dan tablet -->
-      <div class="col-lg-9 offset-lg-2 col-md-10 offset-md-1">
-         <div class="card">
-            <div class="card-header d-flex align-items-center flex-wrap">
-               <h2>Struktur Organisasi</h2>
+    <div class="row justify-content-center">
+        <!-- Mengatur ulang lebar kolom dan offset untuk desktop dan tablet -->
+        <div class="col-lg-9 offset-lg-2 col-md-10 offset-md-1">
+            <div class="card">
+                <div class="card-header d-flex align-items-center flex-wrap">
+                    <h2>Struktur Organisasi</h2>
+                </div>
+                <div class="card-body">
+                    <div id="chartDiv1" style="height: 600px;"></div>
+                </div>
             </div>
-            <div class="card-body">
-               <div id="chartDiv1" style="height: 600px;"></div>
+            <!-- Tabel, API dari Get Perangkat Desa -->
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">Detail</h5>
+                </div>
+                <div class="card-body">
+                    <table id="scroll-horizontal" class="table nowrap align-middle" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th class="w-30">Nama Barang</th>
+                                <th>Kode Barang</th>
+                                <th>Register</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Laptop ASUS</td>
+                                <td>3070102</td>
+                                <td>3070102 | 00001</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Kantor Desa</td>
+                                <td>3070102</td>
+                                <td>3070102 | 00001</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </div>
 
 
-    <script src="https://code.jscharting.com/latest/jscharting.js"></script>
-    <script>
+<script src="https://code.jscharting.com/latest/jscharting.js"></script>
+<script>
     // JS 
     var chart = JSC.chart('chartDiv1', {
         debug: false,
@@ -98,4 +130,4 @@
             ]
         }]
     });
-    </script>
+</script>
